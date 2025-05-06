4b22246d17ba5849ca366898490c08c8f69929cd
Sabio <gamester.sad@gmail.com>
2025-05-06 20:58:53 +0100

    Initial commit

---
+++ src/main/java/net/sabio/abyssium/Abyssium.java
@@ -0,0 +1,16 @@
+package net.sabio.abyssium;
+
+import net.fabricmc.api.ModInitializer;
+import net.sabio.abyssium.item.ModItems;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Abyssium implements ModInitializer {
+    public static final String MOD_ID = "abyssium_mod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        ModItems.initialize();
+    }
+}
---
+++ src/main/java/net/sabio/abyssium/item/ModItems.java
@@ -0,0 +1,35 @@
+package net.sabio.abyssium.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.sabio.abyssium.Abyssium;
+import net.minecraft.item.Item;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+
+import java.util.function.Function;
+
+public class ModItems {
+    public static final Item ABYSSIUM = register("abyssium", Item::new, new Item.Settings());
+
+    public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
+        // Create the item key.
+        RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Abyssium.MOD_ID, name));
+
+        // Create the item instance.
+        Item item = itemFactory.apply(settings.registryKey(itemKey));
+
+        // Register the item.
+        Registry.register(Registries.ITEM, itemKey, item);
+
+        return item;
+    }
+
+    public static void initialize() {
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
+                .register((itemGroup) -> itemGroup.add(ModItems.ABYSSIUM));
+    }
+}
